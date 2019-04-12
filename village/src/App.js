df7544fb62547363ffe31ace78735382cd1df103
ayunas <ayunas@gmail.com>
2019-04-12 10:27:13 -0500

    set up project, and added a sample smurf to state using setState() in componentDidMount of <App> to test the state and rendering

--- village/src/App.js
+++ village/src/App.js
@@ -11,10 +11,20 @@ class App extends Component {
       smurfs: [],
     };
   }
+
+  componentDidMount() {
+    this.setState({
+      smurfs: [{name: 'smurfette', age: 99, height: '10cm'}]
+    })
+
+  }
+  
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <SmurfForm />
